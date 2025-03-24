acc6e1e55f22f5f6d6a244ed6638841554d40ee5
7uhinn <tuhin.sarkar98@gmail.com>
2025-03-25 02:38:41 +0530

    Completed assignment 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let finalArray = [];
+
+    for (const item of arr) {
+        if(!args.includes(item)){
+            finalArray.push(item);
+        }
+    }
+
+    return finalArray;
 };
 
 // Do not edit below this line
