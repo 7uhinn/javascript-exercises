a907c045294ae37421e8734b41996c080f805534
7uhinn <tuhin.sarkar98@gmail.com>
2025-03-25 02:01:14 +0530

    Completed assignment 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let textLength = text.length;
+    let str = '';
 
+    for (let i = textLength-1; i >= 0; i--) {
+        str += text[i];
+    }
+
+    return str;
 };
 
 // Do not edit below this line
